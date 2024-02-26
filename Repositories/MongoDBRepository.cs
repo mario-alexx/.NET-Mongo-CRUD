e92077be772742a04905e5018b60f4446684738a
mario_alex <marioatoroh@gmail.com>
2024-02-26 10:48:52 -0300

    First commit

---
+++ Repositories/MongoDBRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace MVCCore.MongoDB.CRUD.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public MongoDBRepository()
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+
+            db = client.GetDatabase("MusicCatalog");
+        }
+    }
+}
